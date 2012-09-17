86073c1930b21282dcf78cf77c37bfd0897994a7
cody <nathancodymann@gmail.com>
2012-09-17 14:20:19 -0500

    added header to file, small changes

--- robot/CameraServo.h
+++ robot/CameraServo.h
@@ -1,11 +1,14 @@
+/*
+ * Library for servo interaction for firepenguindiscopanda
+ * Author: Nathan Mann, Sam Pierce, Nicholas Roge
+ * Copyright 2012, All rights reserved.
+ */
 #ifndef cameraservo_h
 #define cameraservo_h
 #include "Arduino.h"
 class CameraServo
 {
-	
+	public:
+		CameraServo(int pin);
 }
-
-
-
 #endif
