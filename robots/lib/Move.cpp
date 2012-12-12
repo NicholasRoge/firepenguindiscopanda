7edec7e3f980a51163c3a2f4bce83dafa9a63b5f
nathanmann <nathancodymann@gmail.com>
2012-12-12 15:17:02 -0600

    Update robots, code documentation change.

--- robots/lib/Move.cpp
+++ robots/lib/Move.cpp
@@ -9,6 +9,12 @@ int Move::_pinMotorTwo = 0;
 int Move::_stopNum     = 127; //stop nu
 int Move::_forwardNum  = 155; //255 max
 int Move::_backwardNum = 100; //0 minim
+/* For this project these torque values are not actually quite correct.
+ * With the weight of the chassis and hardware we used it turned out the torque numbers
+ * had to be lowered quite a bit, as if the robot moved to quickly or jerkily it would
+ * cause the capture software to not be able to update the new location of the camera, 
+ * causing the capture to fail.
+ */
 
 Move::Move() {
 	motorOne.attach(_pinMotorOne);
